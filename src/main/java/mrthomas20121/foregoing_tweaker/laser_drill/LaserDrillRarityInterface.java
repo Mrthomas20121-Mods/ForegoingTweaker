57af0d32425e85a94e049752cbb09a3cd71eedef
Mrthomas20121 <thomas77300@gmail.com>
2021-11-04 10:06:29 +0100

    Update LaserDrillRarityInterface.java

--- src/main/java/mrthomas20121/foregoing_tweaker/laser_drill/LaserDrillRarityInterface.java
+++ src/main/java/mrthomas20121/foregoing_tweaker/laser_drill/LaserDrillRarityInterface.java
@@ -1,6 +1,7 @@
 package mrthomas20121.foregoing_tweaker.laser_drill;
 
 import com.blamejared.crafttweaker.api.annotations.ZenRegister;
+import com.blamejared.crafttweaker.api.brackets.CommandStringDisplayable;
 import com.blamejared.crafttweaker_annotations.annotations.ZenWrapper;
 import com.buuz135.industrial.recipe.LaserDrillRarity;
 import org.openzen.zencode.java.ZenCodeType;
@@ -8,12 +9,9 @@ import org.openzen.zencode.java.ZenCodeType;
 @ZenRegister
 @ZenCodeType.Name("mods.foregoing_tweaker.laser_drill.LaserDrillRarityInterface")
 @ZenWrapper(wrappedClass = "com.buuz135.industrial.recipe.LaserDrillRarity", displayStringFormat = "%.getCommandString()", creationMethodFormat = "new CTLaserDrillRarity(%s)", implementingClass = "mrthomas20121.foregoing_tweaker.laser_drill.CTLaserDrillRarity")
-public interface LaserDrillRarityInterface {
+public interface LaserDrillRarityInterface extends CommandStringDisplayable {
 
     @ZenCodeType.Method
     @ZenCodeType.Caster(implicit = true)
     LaserDrillRarity getInternal();
-
-    @ZenCodeType.Method
-    String getCommandString();
 }
